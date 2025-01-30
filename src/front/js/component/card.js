2813d9e2c609e524ddb5802b83e01209929056de
Agustín Demontel <53986476+ademontel@users.noreply.github.com>
2025-01-30 18:52:44 +0000

    Added the basic DB model with relationships

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -3,9 +3,9 @@ import cardimg from "../../img/card-example.jpg";
 
 export const Card = () => {
     return (
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card mx-auto" style={{ width: "18rem" }}>
             <img src={cardimg} alt="Card example" />
-            <div className="card-body">Producto 1</div>
+            <div className="card-body">Producto 1 <i className="fa-solid fa-cart-shopping"></i></div>
         </div>
     );
 };
