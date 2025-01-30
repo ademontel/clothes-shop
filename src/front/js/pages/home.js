3e68dd96304b95155fdf04d0df7b8a13cd36538e
Agustín Demontel <53986476+ademontel@users.noreply.github.com>
2025-01-30 17:22:56 +0000

    Added a little bit of front-ish shape

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,19 +2,27 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Card } from "../component/card";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Fashion store!!</h1>
+		<div className="container-fluid">
+			<h1>Productos nuevos</h1>
 			<p>
 			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+			<div className="row">
+				<div className="col-12 col-md-4">
+					<Card />
+				</div>
+				<div className="col-12 col-md-4">
+					<Card />
+				</div>
+				<div className="col-12 col-md-4">
+					<Card />
+				</div>
 			</div>
-		
 		</div>
 	);
 };
